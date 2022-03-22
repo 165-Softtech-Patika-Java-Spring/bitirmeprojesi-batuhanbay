5e984ecf5d6680b143c490092834953ea4fa2426
batuhanbay <bayoglubatuhan@gmail.com>
2022-03-22 21:30:30 +0300

    Import new lodaing initial data.

---
+++ bitirme/src/test/java/com/softtechbootcamp/bitirme/app/resources/import_product.sql
@@ -0,0 +1,4 @@
+INSERT INTO prd_product (id, created_by, updated_by,initial_price, kdv_amount,last_price, name,id_product_type) VALUES ('1','1','1',100,1,101,'Test1','1');
+INSERT INTO prd_product (id, created_by, updated_by,initial_price, kdv_amount,last_price, name,id_product_type) VALUES ('2','1','1',100,1,101,'Test2','1');
+INSERT INTO prd_product (id, created_by, updated_by,initial_price, kdv_amount,last_price, name,id_product_type) VALUES ('3','1','1',100,1,101,'Test3','1');
+INSERT INTO prd_product (id, created_by, updated_by,initial_price, kdv_amount,last_price, name,id_product_type) VALUES ('4','1','1',100,1,101,'Test4','1');
---
+++ bitirme/src/test/java/com/softtechbootcamp/bitirme/app/resources/import_product_type.sql
@@ -0,0 +1,6 @@
+INSERT INTO prt_product_type (id, kdv, name) VALUES ('1', '1', 'FOOD');
+INSERT INTO prt_product_type (id, kdv, name) VALUES ('2', '8', 'STATIONARY');
+INSERT INTO prt_product_type (id, kdv, name) VALUES ('3', '8', 'CLOTHING');
+INSERT INTO prt_product_type (id, kdv, name) VALUES ('4', '18', 'TECHNOLOGY');
+INSERT INTO prt_product_type (id, kdv, name) VALUES ('5', '18', 'CLEANING');
+INSERT INTO prt_product_type (id, kdv, name) VALUES ('6', '18', 'OTHERS');
