9cd866da01732d57ad1d4d5bc5a28854ba3af4ac
batuhanbay <bayoglubatuhan@gmail.com>
2022-03-24 20:18:30 +0300

    Add swagger documentation to settings controller.

--- bitirme/src/main/java/com/softtechbootcamp/bitirme/app/set/controller/SetSettingsController.java
+++ bitirme/src/main/java/com/softtechbootcamp/bitirme/app/set/controller/SetSettingsController.java
@@ -1,11 +1,17 @@
 package com.softtechbootcamp.bitirme.app.set.controller;
 
 import com.softtechbootcamp.bitirme.app.gen.dto.GeneralResponse;
+import com.softtechbootcamp.bitirme.app.prd.dto.PrdProductDto;
 import com.softtechbootcamp.bitirme.app.prd.dto.PrdProductResponseDto;
 import com.softtechbootcamp.bitirme.app.set.dto.SetSettingsDto;
 import com.softtechbootcamp.bitirme.app.set.service.SetSettingsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +22,32 @@ import java.util.List;
 public class SetSettingsController {
     private SetSettingsService setSettingsService;
 
+    @Operation(tags = "Settings", description = "This method find all settings", summary = "Find all settings")
     @GetMapping
     public ResponseEntity<?> findAll(){
         List<SetSettingsDto> setSettingsDtoList = setSettingsService.findAll();
         return ResponseEntity.ok(GeneralResponse.of(setSettingsDtoList));
     }
 
+    @Operation(
+            tags = "Settings", description = "This method update a setting.", summary = "Update a setting",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = SetSettingsDto.class
+                                    ),
+                                    examples = {
+                                            @ExampleObject(
+                                                    value = "{\"key\": \"savedDirectoryPath\",\"value\": \"D:\\\\jasper_reports_pdf\\\\\"}"
+                                            )
+                                    }
+                            ),
+                    }
+            )
+    )
+    @Validated
     @PutMapping
     public ResponseEntity<?> updateSettings(@RequestBody SetSettingsDto setSettingsDto){
         SetSettingsDto savedSetSettingsDto = setSettingsService.updateSettings(setSettingsDto);
